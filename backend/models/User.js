4a46f0ed52ea568b1c4da985f210e72784043f74
SudeK <sudekrdnz@ymail.com>
2025-08-15 15:33:46 +1000

    made change in User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,6 +14,7 @@ userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
