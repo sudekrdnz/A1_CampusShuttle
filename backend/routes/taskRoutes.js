8247c12489441aa8548aee61dac4ce72228ebe6b
Sude Karadeniz <sudekrdnz@Sudes-MBP.modem>
2025-08-16 19:18:48 +1000

    Reference base path in router REST methods

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,9 +3,9 @@ const { getTasks, addTask, updateTask, deleteTask } = require('../controllers/ta
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/tasks', protect, getTasks);
-router.post('/tasks', protect, addTask);
-router.put('/tasks', protect, updateTask);
-router.delete('/tasks', protect, deleteTask)
+router.get('/', protect, getTasks);
+router.post('/', protect, addTask);
+router.put('/', protect, updateTask);
+router.delete('/', protect, deleteTask)
 
 module.exports = router;
