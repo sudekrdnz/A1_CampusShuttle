5aec6c6a230752eebf9621b93be93cdaf3a59022
Sude Karadeniz <sudekrdnz@Sudes-MacBook-Pro.local>
2025-08-28 07:51:19 +1000

    change baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@
 
   const axiosInstance = axios.create({
     // baseURL: 'http://localhost:5001', // local
-    baseURL: 'http://3.106.55.115:5001', // live
+    baseURL: 'http://3.27.221.73:5001', // live
     headers: { 'Content-Type': 'application/json' },
   });
 
